634af45a55e7404039a44a7ab2f15bd663c5fa35
Pedro Delgadillo <PJDelgadillo@student.fullsail.edu>
2024-04-13 00:38:56 -0500

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  __APP_ENV__: process.env.VITE_VERCEL_ENV,
+})
