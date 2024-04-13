5084682dd7b2db532ade64ed57a708e4cc3953b7
Pedro Delgadillo <PJDelgadillo@student.fullsail.edu>
2024-04-13 00:34:40 -0500

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base:'/Journal-app/',
-  __APP_ENV__: process.env.VITE_VERCEL_ENV,
-})
