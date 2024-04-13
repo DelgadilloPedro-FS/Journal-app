638eafc5ff03b211fce62f08e3eb7f1e2dca6305
Pedro Delgadillo <PJDelgadillo@student.fullsail.edu>
2024-04-13 00:11:19 -0500

    update for vercel

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/Journal-app/'
+  base:'/Journal-app/',
+  __APP_ENV__: process.env.VITE_VERCEL_ENV,
 })
