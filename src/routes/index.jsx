9fa35867a1ab7e0d92b3aaad76628150fba71ee7
Pedro Delgadillo <PJDelgadillo@student.fullsail.edu>
2024-04-14 17:56:06 -0500

    add landing page content

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,3 +1,13 @@
 export default function Index() {
-  return <p id="zero-state">Landing page coming</p>;
+  return (
+    <div className="items-center justify-center bg-gray-100 p-6 shadow-lg">
+      <div className="flex justify-center items-center mb-8">
+        <h1 className="text-3xl font-bold text-gray-800">Memory Lane</h1>
+      </div>
+      <p className="text-gray-700 text-lg mb-8">
+        Capture your thoughts and experiences in a beautiful and easy-to-use
+        journal.
+      </p>
+    </div>
+  );
 }
